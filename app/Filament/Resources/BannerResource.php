99aab9dc11959f81e13a5e4d8ce59c048fd12634
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-03-28 19:07:16 +0530

    banner resources

---
+++ app/Filament/Resources/BannerResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BannerResource\Pages;
+use App\Filament\Resources\BannerResource\RelationManagers;
+use App\Models\Banner;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BannerResource extends Resource
+{
+    protected static ?string $model = Banner::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('image')
+                    ->image()
+                    ->required(),
+                Forms\Components\TextInput::make('highlight_text')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('button_name')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('button_url')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\DateTimePicker::make('time_from'),
+                Forms\Components\DateTimePicker::make('time_to'),
+                Forms\Components\TextInput::make('priority')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\Toggle::make('is_visible')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('highlight_text')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('button_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('button_url')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('time_from')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('time_to')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('priority')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_visible')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBanners::route('/'),
+            'create' => Pages\CreateBanner::route('/create'),
+            'edit' => Pages\EditBanner::route('/{record}/edit'),
+        ];
+    }
+}
