ecd014e5d15485d9c4ceb52973d3329cc721b733
Nityanand <nityanandsharma.0000@gmail.com>
2024-04-27 20:31:35 +0530

    fixed typing text

--- resources/views/livewire/home-banner.blade.php
+++ resources/views/livewire/home-banner.blade.php
@@ -2,14 +2,23 @@
     <div class="left_banner_text">
         <div><span style="color:rgb(0 0 0);font-size:46px;font-weight:500 !important;line-height:28px">Create budget</span><span style="font-size:46px;font-weight:500 !important;line-height:28px"> &#38;</span> {{--  Best Health--}}</div>
         <span style="color:rgb(0 0 0);font-size:46px;font-weight:500 !important;line-height:28px">Get Amazing </span>
-        <div class="slidingVertical">
-           @foreach ($services as $key => $service )
-            <span  style="color: {{$colors[$key]}}">{{strtoupper($service->name)}}</span>
+        <div class="slidingVertical wrapper">
+           
+                
+                <ul class="dynamic-txts">
+                @foreach ($services as $key => $service )
+                <li>
+                    
+                        <span  style="line-height:normal;color: {{$colors[$key]}}">{{strtoupper($service->name)}}</span></li>
+
             @endforeach
+            </ul>
             {{-- <span>LEAD GENERATION</span>
             <span>DIGITAL MARKETING</span>
             <span>SOFTWARE</span>
             <span>Application</span> --}}
+           
+           
         </div>
     </div>
 
