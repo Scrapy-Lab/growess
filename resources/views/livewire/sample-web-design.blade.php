b906027542ef8424d2f5e6d52f3b467b25d8797f
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-07-26 17:00:24 +0530

    button color done

--- resources/views/livewire/sample-web-design.blade.php
+++ resources/views/livewire/sample-web-design.blade.php
@@ -6,13 +6,13 @@
         <p class="code_assessment_job_details_description">find a role that fits your carrier aspirators.</p>
         <div class="code_assessment_job_details_box-container">
             <ul class="tab_box">
-                <li class=" select_mode @if ($static) select  @endif  fa fa-search" wire:click="web_type('static')" data-tab="tab01">&nbsp;Static
+                <li class=" btn_color1 select_mode @if ($static) select  @endif  fa fa-search" wire:click="web_type('static')" data-tab="tab01" style=" ;">&nbsp;Static
                     Website</li>
-                <li class="select_mode  @if ($dynamic) select  @endif" wire:click="web_type('dynamic')" data-tab="tab02">&nbsp;Dynamic Website</li>
+                <li class="btn_color2 select_mode  @if ($dynamic) select  @endif" wire:click="web_type('dynamic')" data-tab="tab02" style=" ;">&nbsp;Dynamic Website</li>
                 {{-- <li class="select_mode" data-tab="tab03">&nbsp;</li> --}}
-                <li class="select_mode fa fa-home @if ($brochure) select  @endif " wire:click="web_type('brochure')" data-tab="tab04">&nbsp;Brochure
+                <li class="btn_color3 select_mode fa fa-home @if ($brochure) select  @endif " wire:click="web_type('brochure')" data-tab="tab04" style=" ">&nbsp;Brochure
                     Website</li>
-                <li class="select_mode @if ($ecom) select  @endif" wire:click="web_type('ecom')" data-tab="tab05">&nbsp;E-Commerece Website</li>
+            <li class="btn_color4 select_mode @if ($ecom) select  @endif" wire:click="web_type('ecom')" data-tab="tab05" style="  ;">&nbsp;E-Commerece Website</li>
             </ul>
         </div>
     </div>
@@ -27,29 +27,29 @@
         {{-- <div class="web_type_static"> --}}
             <div class="job_details">
                 <div class="code_assessment_job_details_box1">
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color1">
                         Construction
                         <div class="jobs_category_description">Software development &nbsp;&gt;</div>
                     </div>
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color2">
                         Restaurant
                         <div class="jobs_category_description">computer science &nbsp;&gt;</div>
                     </div>
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color3">
                         School / College
                         <div class="jobs_category_description">marketing &nbsp;&gt;</div>
                     </div>
                 </div>
                 <div class="code_assessment_job_details_box2">
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color4">
                         Shop
                         <div class="jobs_category_description">Software development &nbsp;&gt;</div>
                     </div>
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color1">
                         Portfolio
                         <div class="jobs_category_description">computer science &nbsp;&gt;</div>
                     </div>
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color2">
                         Entartainment
                         <div class="jobs_category_description">marketing &nbsp;&gt;</div>
                     </div>
@@ -63,30 +63,30 @@
         {{-- <div class="web_type_dynamic"> --}}
             <div class="job_details">
                 <div class="code_assessment_job_details_box1">
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color1">
                         dynamic   Construction
                         <div class="jobs_category_description">Software development &nbsp;&gt;</div>
                     </div>
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color2">
                         dynamic Restaurant
                         <div class="jobs_category_description">computer science &nbsp;&gt;</div>
                     </div>
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color3">
                         dynamic  School / College
                         <div class="jobs_category_description">marketing &nbsp;&gt;</div>
                     </div>
                 </div>
                 <div class="code_assessment_job_details_box2">
-                    
-                    <div class="jobs_category">
+
+                    <div class="jobs_category btn_color4">
                         Shop
                         <div class="jobs_category_description">Software development &nbsp;&gt;</div>
                     </div>
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color1">
                         Portfolio
                         <div class="jobs_category_description">computer science &nbsp;&gt;</div>
                     </div>
-                    <div class="jobs_category">
+                    <div class="jobs_category btn_color2">
                         Entartainment
                         <div class="jobs_category_description">marketing &nbsp;&gt;</div>
                     </div>
@@ -99,29 +99,29 @@
         {{-- <div class="web_type_dynamic"> --}}
         <div class="job_details">
             <div class="code_assessment_job_details_box1">
-                <div class="jobs_category">
+                <div class="jobs_category btn_color1">
                     brochure   Construction
                     <div class="jobs_category_description">Software development &nbsp;&gt;</div>
                 </div>
-                <div class="jobs_category">
+                <div class="jobs_category btn_color2">
                     brochure  Restaurant
                     <div class="jobs_category_description">computer science &nbsp;&gt;</div>
                 </div>
-                <div class="jobs_category">
+                <div class="jobs_category btn_color3">
                     brochure  School / College
                     <div class="jobs_category_description">marketing &nbsp;&gt;</div>
                 </div>
             </div>
             <div class="code_assessment_job_details_box2">
-                <div class="jobs_category">
+                <div class="jobs_category btn_color4">
                     brochure   Shop
                     <div class="jobs_category_description">Software development &nbsp;&gt;</div>
                 </div>
-                <div class="jobs_category">
+                <div class="jobs_category btn_color1">
                     Portfolio
                     <div class="jobs_category_description">computer science &nbsp;&gt;</div>
                 </div>
-                <div class="jobs_category">
+                <div class="jobs_category btn_color2">
                     Entartainment
                     <div class="jobs_category_description">marketing &nbsp;&gt;</div>
                 </div>
@@ -131,30 +131,30 @@
     {{-- Dynamic website --}}
     @if ($ecom)
         <div class="job_details">
-            <div class="code_assessment_job_details_box1"> 
-                <div class="jobs_category">
+            <div class="code_assessment_job_details_box1">
+                <div class="jobs_category btn_color1">
                     ecom    Construction
                     <div class="jobs_category_description">Software development &nbsp;&gt;</div>
                 </div>
-                <div class="jobs_category">
+                <div class="jobs_category btn_color2">
                     ecom  Restaurant
                     <div class="jobs_category_description">computer science &nbsp;&gt;</div>
                 </div>
-                <div class="jobs_category">
+                <div class="jobs_category btn_color3">
                     ecom  School / College
                     <div class="jobs_category_description">marketing &nbsp;&gt;</div>
                 </div>
             </div>
             <div class="code_assessment_job_details_box2">
-                <div class="jobs_category">
+                <div class="jobs_category btn_color4">
                     Shop
                     <div class="jobs_category_description">Software development &nbsp;&gt;</div>
                 </div>
-                <div class="jobs_category">
+                <div class="jobs_category btn_color1">
                     Portfolio
                     <div class="jobs_category_description">computer science &nbsp;&gt;</div>
                 </div>
-                <div class="jobs_category">
+                <div class="jobs_category btn_color2">
                     Entartainment
                     <div class="jobs_category_description">marketing &nbsp;&gt;</div>
                 </div>
@@ -172,9 +172,9 @@
             $('.select_mode').click(function() {
                 // Change color to blue on click
 
-                $('.select_mode').css('background-color', '#ffffff');
+                // $('.select_mode').css('background-color', '#ffffff');
                 $('.select_mode').css('color', 'black');
-                $(this).css('background-color', '#0862ab');
+                // $(this).css('background-color', '#0862ab');
                 $(this).css('color', 'white');
                 console.log("fasdasdasda");
             });
